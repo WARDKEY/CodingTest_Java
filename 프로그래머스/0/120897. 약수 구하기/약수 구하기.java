b15a0a65e57261b60884a2bfee3b0674fbe84e8f
WARDKEY <132131921+WARDKEY@users.noreply.github.com>
2024-08-13 12:51:50 +0900

    [level 0] Title: 약수 구하기, Time: 0.71 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <=n; i++){
+            if(n % i == 0){
+                list.add(i);
+            }
+        }
+        
+        
+        int[] arr = new int[list.size()];
+        
+        for(int i = 0; i < list.size(); i++){
+            arr[i] = list.get(i);
+        }
+        
+        Arrays.sort(arr);
+        
+        return arr;
+    }
+}
