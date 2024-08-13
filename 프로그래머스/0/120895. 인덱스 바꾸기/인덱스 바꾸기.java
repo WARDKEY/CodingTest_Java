21658554c7588911598a9c6325cd214877814c48
WARDKEY <132131921+WARDKEY@users.noreply.github.com>
2024-08-13 12:09:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.19 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        StringBuilder answer = new StringBuilder();
+        String[] arr = my_string.split("");
+        String temp = "ee";
+        
+        temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        
+        for(String s : arr){
+            answer.append(s);
+        }
+        
+        
+        
+        return answer.toString();
+    }
+}
