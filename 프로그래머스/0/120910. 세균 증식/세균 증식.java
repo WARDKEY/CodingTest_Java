ab3a083bfe26a1250394e4664c289179f312bc4c
WARDKEY <132131921+WARDKEY@users.noreply.github.com>
2024-08-20 00:01:34 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        for(int i = 0; i < t; i++){
+            n += n;
+        }
+        answer = n;
+        return answer;
+    }
+}
